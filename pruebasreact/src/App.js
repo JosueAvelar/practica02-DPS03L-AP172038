09fc39ec68dda5f5a2c6fb74bc4c9aaf95eacf1c
JosueAvelar <avelarjosue1@gmail.com>
2021-07-30 08:41:39 -0600

    Primera Version Guia02L

---
+++ pruebasreact/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import react from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="App-content">
+        <p>
+          Aqui haremos nuestro To-Do list
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
